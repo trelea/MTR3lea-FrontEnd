509e586f57c2eddfb9b4d819e9bbb5bb179385fc
trelea <treleamarius76@gmail.com>
2024-01-08 00:50:27 +0200

    commit

--- src/components/AccMenu.jsx
+++ src/components/AccMenu.jsx
@@ -24,7 +24,7 @@ export const AccMenu = ({ userMenu, setUserMenu }) => {
                 <div className="border border-gray-300 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-xl mr-2 mt-4 bg-white">
                     <ul className="p-2 px-4">
                         {/**onClick={() => window.location.href = `/user/${data.user_name}`} */}
-                        <Link to={`/user/${data.user_name}`} >
+                        <Link to={`/user/${data.user_name}`} reloadDocument>
                             <li className="text-black hover:bg-slate-200 hover:rounded-lg px-2 py-1 my-1 font-medium text-lg border-b border-slate-200 flex gap-2 items-center" >
                                 <img className="aspect-square h-5 w-5" src="https://img.icons8.com/ios-filled/50/user-male-circle.png" alt="user-male-circle"/>
                                 <p>Profile</p>
@@ -32,14 +32,14 @@ export const AccMenu = ({ userMenu, setUserMenu }) => {
                         </Link>
                         
                         {/**onClick={() => window.location.href = '/accounts/profile/settings'} */}
-                        <Link to={'/accounts/profile/settings'} >
+                        <Link to={'/accounts/profile/settings'} reloadDocument>
                             <li className="text-black hover:bg-slate-200 hover:rounded-lg px-2 py-1 my-1 font-medium text-lg border-b border-slate-200 flex gap-2 items-center" >
                                 <img className="aspect-square h-5 w-5" src="https://img.icons8.com/ios-filled/50/settings.png" alt="settings" />
                                 <p>Edit Profile</p>
                             </li>
                         </Link>
 
-                        <Link>
+                        <Link reloadDocument>
                             <li className="text-black hover:bg-slate-200 hover:rounded-lg px-2 py-1 my-1 font-medium text-lg border-b border-slate-200 flex gap-2 items-center" onClick={() => setUploadPost(true)}>
                                 <img className="aspect-square h-5 w-5" src="https://img.icons8.com/ios/50/upload--v1.png" alt="upload--v1"/>
                                 <p>New Ann...</p>    
@@ -47,7 +47,7 @@ export const AccMenu = ({ userMenu, setUserMenu }) => {
                         </Link>
                             
                         {/**onClick={() => window.location.href = '/signin'} */}
-                        <Link to={'/signin'} >
+                        <Link to={'/signin'} reloadDocument>
                             <li className="text-black hover:bg-slate-200 hover:rounded-lg px-2 py-1 my-1 font-medium text-lg border-b border-slate-200 flex gap-2 items-center" >
                                 <img className="aspect-square h-5 w-5" src="https://img.icons8.com/ios/50/login-rounded-right--v1.png" alt="login-rounded-right--v1"/>
                                 <p>Switch Accounts</p>
@@ -61,7 +61,7 @@ export const AccMenu = ({ userMenu, setUserMenu }) => {
                             </li>
                         </Link>
                         
-                        <Link>
+                        <Link reloadDocument>
                             <li className="text-red-600 hover:bg-slate-200 rounded-lg px-2 py-1 font-medium text-lg flex gap-2 items-center" onClick={e => handleLogOut(e)}>
                                 <img className="aspect-square h-5 w-5" src="https://img.icons8.com/ios/50/logout-rounded-left.png" alt="logout-rounded-left"/>
                                 <p>Logout</p>
