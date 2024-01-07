4505e4535cf962440fa0535aac1aab3f5902b88c
trelea <treleamarius76@gmail.com>
2024-01-08 01:18:46 +0200

    commit

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -5,10 +5,11 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ShareBtn } from "./ShareBtn";
 import { PostOptions } from "./PostOptions";
 
-export const CardPost = ({ post, postOptions }) => {
+export const CardPost = ({ post, postOptions, setAuthenticated }) => {
     const { data } = useContext(UserContext);
     const [copied, setCopied] = useState(false);
     const [previewPostOptions, setPreviewPostOptions] = useState(false);
+    const [isAuthenticated, setAuthenticates] = useState(false);
 
     const [likeStatus, setLikeStatus] = useState({
         userLiked: post.post_likes.includes(data.user_name),
@@ -42,7 +43,7 @@ export const CardPost = ({ post, postOptions }) => {
                 <div className="relative flex justify-between">
                     <div className="flex gap-2 items-center">
                         {/**onClick={() => window.location.href = `/user/${post.user_name}`} */}
-                        <Link to={ data?.user_name && `/user/${post.user_name}` } className="flex gap-2 items-center" reloadDocument preventScrollReset>
+                        <Link to={ data?.user_name ? `/user/${post.user_name}` : setAuthenticated(false) } className="flex gap-2 items-center" reloadDocument preventScrollReset>
                             <img className="rounded-full object-cover h-6 w-6" src={`${process.env.REACT_APP_APIURL}${post.user_thumbnail}`} alt="" />
                             <h1 className="font-semibold text-sm text-gray-700 hover:underline hover:text-blue-700">m/{post.user_name}</h1>
                         </Link>
@@ -91,7 +92,7 @@ export const CardPost = ({ post, postOptions }) => {
                     </div>
 
                     {/**onClick={() => window.location.href = `/post/${post.post_id}`} */}
-                    <Link to={ data?.user_name && `/post/${post.post_id}`} reloadDocument preventScrollReset>
+                    <Link to={ data?.user_name ? `/post/${post.post_id}`: } reloadDocument preventScrollReset>
                         <div className="border border-gray-400 rounded-xl px-2 py-1 flex items-center gap-2 hover:bg-gray-100">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { CardPost } from "../../components/CardPost";
 import { UploadButton } from "../../components/UploadButton.jsx";
 import { useSearchParams } from "react-router-dom";
 import { Pagination } from "../../components/Pagination.jsx";
+import { Modal } from "../../components/Modal.jsx"
 
 
 export const Home = () => {
@@ -11,6 +12,7 @@ export const Home = () => {
     const [posts, setPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isError,   setIsError  ] = useState(false);
+    const [isAuthenticated, setAuthenticated] = useState(true);
 
     useEffect(() => {
         setIsLoading(true);
@@ -30,12 +32,15 @@ export const Home = () => {
     
     return (
         <Loyout>
+            {
+                !isAuthenticated && <Modal/>
+            }
             <div className="bg-slate-100 pt-20 flex flex-col items-center w-screen p-2">
                 <div className="flex flex-col gap-2 w-full sm:w-[95%] md:w-[80%] lg:w-[65%] xl:w-[50%] 2xl:w-[40%]">
                     <UploadButton/>
                     <div className="flex flex-col gap-2">
                         {posts.map((post) => {
-                            return <CardPost key={post.post_id} post={post} postOptions={false}/> 
+                            return <CardPost key={post.post_id} post={post} postOptions={false} setAuthenticated={setAuthenticated} /> 
                         })}
                     </div>
                     <Pagination serachParams={serachParams} setSearchParams={setSearchParams}/>  
