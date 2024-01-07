18046b0892999ff876ef52f224752e906d276c9d
trelea <treleamarius76@gmail.com>
2024-01-08 01:20:40 +0200

    commit

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -68,7 +68,7 @@ export const CardPost = ({ post, postOptions, setAuthenticated }) => {
                 </div>
 
                 {/**onClick={() => window.location.href = `/post/${post.post_id}`} */}
-                <Link to={ data?.user_name && `/post/${post.post_id}`} reloadDocument preventScrollReset>
+                <Link to={ data?.user_name ? `/post/${post.post_id}` : setAuthenticated(false) } reloadDocument preventScrollReset>
                     <div className="pt-2">
                         <h1 className="font-bold text-lg leading-tight">{post.post_title}</h1>
                         <h1 className="font-normal text-sm text-gray-700 pt-1 w-fit hover:underline hover:text-blue-700">Click to read more ...</h1>
@@ -92,7 +92,7 @@ export const CardPost = ({ post, postOptions, setAuthenticated }) => {
                     </div>
 
                     {/**onClick={() => window.location.href = `/post/${post.post_id}`} */}
-                    <Link to={ data?.user_name ? `/post/${post.post_id}`: } reloadDocument preventScrollReset>
+                    <Link to={ data?.user_name ? `/post/${post.post_id}`: setAuthenticated(false) } reloadDocument preventScrollReset>
                         <div className="border border-gray-400 rounded-xl px-2 py-1 flex items-center gap-2 hover:bg-gray-100">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
